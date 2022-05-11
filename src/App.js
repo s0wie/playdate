f7f6e0a1ff13163d6e981ae1854eccc2cf8c241e
s0wie <sowieh@gmail.com>
2022-05-11 13:26:39 +0200

    Set up a playground with forEach loops

--- src/App.js
+++ src/App.js
@@ -16,11 +16,52 @@ document.body.appendChild(app.view);
 
 const Graphics = PIXI.Graphics;
 
-const rectangle = new Graphics();
-rectangle
-  .beginFill(0xaa33bb)
-  .lineStyle(4, 0xffea00, 1)
-  .drawRect(200, 200, 100, 120)
-  .endFill();
+const World = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
 
-app.stage.addChild(rectangle);
+let y = 0;
+let x = 0;
+World.forEach((WorldElement) => {
+  const rectangle = new Graphics();
+  y += 30;
+  rectangle
+    .beginFill(0xf8f5f1)
+    .lineStyle(2, 0xd9d3d0, 1)
+    .drawRect(0, y, 30, 30)
+    .endFill();
+  app.stage.addChild(rectangle);
+
+  WorldElement.forEach((Zeros) => {
+    const rectangle = new Graphics();
+    if (x >= 300) {
+      x = 0;
+    }
+    x += 30;
+    rectangle
+      .beginFill(0xf8f5f1)
+      .lineStyle(2, 0xd9d3d0, 1)
+      .drawRect(x, y, 30, 30)
+      .endFill();
+    app.stage.addChild(rectangle);
+  });
+});
