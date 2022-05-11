7c5ce3c196253ffcb03247fd5601f56a05187ce1
s0wie <sowieh@gmail.com>
2022-05-11 15:24:16 +0200

    changed loops structure and added functions

--- src/App.js
+++ src/App.js
@@ -39,29 +39,58 @@ const World = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
-let y = 0;
-let x = 0;
-World.forEach((WorldElement) => {
+const colors = [
+  '0xf8f5f1',
+  '0xff8d87',
+  '0xfaffb0',
+  '0xc0ffb6',
+  '0xd0ddff',
+  '0xe1bfff',
+  '0xf8bccb',
+  '0xffcd9c',
+];
+
+function draw_box_at(x, y) {
   const rectangle = new Graphics();
-  y += 30;
   rectangle
-    .beginFill(0xf8f5f1)
+    .beginFill(colors[World[y][x]])
     .lineStyle(2, 0xd9d3d0, 1)
-    .drawRect(0, y, 30, 30)
+    .drawRect(x * 30, y * 30, 30, 30)
     .endFill();
   app.stage.addChild(rectangle);
+}
 
-  WorldElement.forEach((Zeros) => {
-    const rectangle = new Graphics();
-    if (x >= 300) {
-      x = 0;
+function draw_world() {
+  for (let y = 0; y < World.length; y++) {
+    for (let x = 0; x < World[0].length; x++) {
+      draw_box_at(x, y);
     }
-    x += 30;
-    rectangle
-      .beginFill(0xf8f5f1)
-      .lineStyle(2, 0xd9d3d0, 1)
-      .drawRect(x, y, 30, 30)
-      .endFill();
-    app.stage.addChild(rectangle);
-  });
-});
+  }
+}
+
+console.log(World.length);
+
+app.ticker.add((delta) => loop(delta));
+
+function loop(delta) {
+  draw_world();
+}
+
+const L = [
+  [0, 1, 0, 0],
+  [0, 1, 0, 0],
+  [0, 1, 1, 0],
+  [0, 0, 0, 0],
+];
+const I = [
+  [0, 1, 0, 0],
+  [0, 1, 0, 0],
+  [0, 1, 0, 0],
+  [0, 1, 0, 0],
+];
+const Z = [
+  [0, 1, 0, 0],
+  [0, 1, 1, 0],
+  [0, 0, 1, 0],
+  [0, 0, 0, 0],
+];
