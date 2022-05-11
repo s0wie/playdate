0d7a0c028d3edcab374909b27299dfa75a866389
s0wie <sowieh@gmail.com>
2022-05-11 16:17:17 +0200

    testing to move object

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ document.body.appendChild(app.view);
 
 const Graphics = PIXI.Graphics;
 
-const World = [
+let World = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -50,6 +50,30 @@ const colors = [
   '0xffcd9c',
 ];
 
+let ShapePositionX = 4;
+let ShapePositionY = 0;
+
+const L = [
+  [0, 1, 0, 0],
+  [0, 1, 0, 0],
+  [0, 1, 1, 0],
+  [0, 0, 0, 0],
+];
+const I = [
+  [0, 2, 0, 0],
+  [0, 2, 0, 0],
+  [0, 2, 0, 0],
+  [0, 2, 0, 0],
+];
+const Z = [
+  [0, 3, 0, 0],
+  [0, 3, 3, 0],
+  [0, 0, 3, 0],
+  [0, 0, 0, 0],
+];
+
+const shapes = [L, I, Z];
+
 function draw_box_at(x, y) {
   const rectangle = new Graphics();
   rectangle
@@ -68,29 +92,24 @@ function draw_world() {
   }
 }
 
-console.log(World.length);
+function add_shape_to_world(shape) {
+  for (let y = 0; y < shape.length; y++) {
+    for (let x = 0; x < shape[0].length; x++) {
+      if (shape[y][x] != 0) {
+        if (ShapePositionY + y < 20) {
+          World[y + ShapePositionY][x + ShapePositionX] = shape[y][x];
+        }
+      }
+    }
+  }
+}
 
+console.log(World);
 app.ticker.add((delta) => loop(delta));
 
 function loop(delta) {
+  add_shape_to_world(Z);
+  ShapePositionY += 1;
+
   draw_world();
 }
-
-const L = [
-  [0, 1, 0, 0],
-  [0, 1, 0, 0],
-  [0, 1, 1, 0],
-  [0, 0, 0, 0],
-];
-const I = [
-  [0, 1, 0, 0],
-  [0, 1, 0, 0],
-  [0, 1, 0, 0],
-  [0, 1, 0, 0],
-];
-const Z = [
-  [0, 1, 0, 0],
-  [0, 1, 1, 0],
-  [0, 0, 1, 0],
-  [0, 0, 0, 0],
-];
